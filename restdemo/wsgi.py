693a06b9a624c2ddc2044d86d0ff74146658193a
Helen Lai <hsiangju.lai.95316@gmail.com>
2023-12-16 13:53:27 +0800

    edit wsgi.py

--- restdemo/wsgi.py
+++ restdemo/wsgi.py
@@ -3,10 +3,18 @@ import sys
 
 sys.path.insert(0, os.getcwd())
 
-from restdemo import create_app
+from restdemo import create_app, db
 
 # Create an application instance that web servers can use. We store it as
 # "application" (the wsgi default) and also the much shorter and convenient
 # "app".
 
 application = create_app(config_name='production')
+
+if not db.has_table("user"):
+    with application.app_context():
+        db.drop_all()
+        db.create_all()
+        application.logger.info('Initialized the database!')
+else:
+    application.logger.info('Database already contains the users table.')
